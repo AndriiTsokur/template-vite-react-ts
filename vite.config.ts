daec39b8ddae9d94bb89c3ec4abae8a504e914a1
Andrii Tsokur <106312047+AndriiTsokur@users.noreply.github.com>
2024-04-05 19:41:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@src': path.resolve(__dirname, 'src'),
-			'@components': path.resolve(__dirname, 'src/components'),
+			'@': path.resolve(__dirname, './src/'),
+			components: `${path.resolve(__dirname, './src/components/')}`,
 		},
 	},
 	base: '/template-vite-react-ts/',
